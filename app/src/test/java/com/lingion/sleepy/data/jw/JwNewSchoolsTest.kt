745f416709b1ae0104fc55a7067ee9c73064bc90
lingion <lingion@hrbeu.edu.cn>
2026-09-19 15:59:03 +0800

    test: update cf_new routing gates
    Include the new cf_new school in routing checks and advance count contracts for the added school and protocol type.\n\nVerify: gradle :app:testDebugUnitTest --tests affected routing and parser classes (pass)\nref #50

--- app/src/test/java/com/lingion/sleepy/data/jw/JwNewSchoolsTest.kt
+++ app/src/test/java/com/lingion/sleepy/data/jw/JwNewSchoolsTest.kt
@@ -217,7 +217,7 @@ class JwNewSchoolsTest {
             JwProtocol.TYPE_EAMS5, JwProtocol.TYPE_SEU, JwProtocol.TYPE_ZJU,
             JwProtocol.TYPE_USTC, JwProtocol.TYPE_SCU, JwProtocol.TYPE_NEU, JwProtocol.TYPE_BJTU,
             JwProtocol.TYPE_WHUT, JwProtocol.TYPE_CLASSIC_EAMS, JwProtocol.TYPE_YETHAN,
-            JwProtocol.TYPE_XJU_POST
+            JwProtocol.TYPE_XJU_POST, JwProtocol.TYPE_CF_NEW
         )
         val pendingTypes = listOf(
             "com.lingion.sleepy.data.jw.JwChengFangParser" to "cf",
